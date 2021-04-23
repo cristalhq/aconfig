1148d1bd72f6ac6845783ee8ef9f256d4843554c
Oleg Kovalov <oleg@hey.com>
2021-04-23 11:06:43 +0200

    revert if with FileFlag check (#69)

--- aconfig.go
+++ aconfig.go
@@ -201,7 +201,7 @@ func (l *Loader) loadConfig() error {
 
 func (l *Loader) parseFlags() error {
 	// TODO: too simple?
-	if l.flagSet.Parsed() || l.config.SkipFlags || l.config.FileFlag == "" {
+	if l.flagSet.Parsed() || l.config.SkipFlags {
 		return nil
 	}
 	return l.flagSet.Parse(l.config.Args)
